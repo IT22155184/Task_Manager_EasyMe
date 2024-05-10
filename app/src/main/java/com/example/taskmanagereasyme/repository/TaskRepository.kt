23a28deeb03d17973b2599d2b1c3b0cec129b69e
hiranya21 <128223556+hiranya21@users.noreply.github.com>
2024-05-11 01:19:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/taskmanagereasyme/repository/TaskRepository.kt
@@ -0,0 +1,15 @@
+package com.example.taskmanagereasyme.repository
+
+import com.example.taskmanagereasyme.database.TaskDatabase
+import com.example.taskmanagereasyme.model.Task
+
+
+class TaskRepository(private val db: TaskDatabase) {
+
+    suspend fun insertTask(task: Task) = db.getTaskDao().insertTask(task)
+    suspend fun updateTask(task: Task) = db.getTaskDao().updateTask(task)
+    suspend fun deleteTask(task: Task) = db.getTaskDao().deleteTask(task)
+
+    fun getAllTasks() = db.getTaskDao().getAllTasks()
+    fun searchTask(query: String?) = db.getTaskDao().searchTask(query)
+}

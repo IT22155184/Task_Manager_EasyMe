b48c590a0ab5d6d81a6b093bdf99517fc421aa1a
hiranya21 <128223556+hiranya21@users.noreply.github.com>
2024-05-11 11:51:39 +0530

    commit#2

--- app/src/main/java/com/example/taskmanagereasyme/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagereasyme/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.taskmanagereasyme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.taskmanagereasyme.database.TaskDatabase
+import com.example.taskmanagereasyme.repository.TaskRepository
+import com.example.taskmanagereasyme.viewmodel.TaskViewModel
+import com.example.taskmanagereasyme.viewmodel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var taskViewModel: TaskViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViewModel()
+    }
+
+    private fun setupViewModel(){
+        val taskRepository = TaskRepository(TaskDatabase(this))
+        val viewModelProviderFactory = TaskViewModelFactory(application, taskRepository)
+        taskViewModel = ViewModelProvider(this, viewModelProviderFactory)[TaskViewModel::class.java]
     }
 }
---
+++ app/src/main/java/com/example/taskmanagereasyme/viewmodel/TaskViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.taskmanagereasyme.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.taskmanagereasyme.model.Task
+import com.example.taskmanagereasyme.repository.TaskRepository
+import kotlinx.coroutines.launch
+
+class TaskViewModel(app: Application, private val taskRepository: TaskRepository): AndroidViewModel(app) {
+
+    fun addTask(task: Task) =
+        viewModelScope.launch {
+            taskRepository.insertTask(task)
+        }
+
+    fun updateTask(task: Task) =
+        viewModelScope.launch {
+            taskRepository.updateTask(task)
+        }
+
+    fun deleteTask(task: Task) =
+        viewModelScope.launch {
+            taskRepository.deleteTask(task)
+        }
+
+    fun getAllTasks() = taskRepository.getAllTasks()
+
+    fun searchTask(query: String?) =
+        taskRepository.searchTask(query)
+}
